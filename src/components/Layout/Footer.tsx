bbb7d4dd7937576d2599417f33c89afe4c637125
Developer <dev@afrivibesafaris.com>
2026-03-19 16:52:19 +0300

    Integration: Added Tawk.to widget and pre-written WhatsApp inquiry message in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -16,7 +16,7 @@ const Footer: React.FC = () => {
             <a href="#" aria-label="Facebook"><Facebook size={20} /></a>
             <a href="#" aria-label="Instagram"><Instagram size={20} /></a>
             <a href="#" aria-label="Twitter"><Twitter size={20} /></a>
-            <a href="https://wa.me/254742009497" target="_blank" rel="noreferrer" aria-label="WhatsApp" style={{ color: "var(--color-primary)" }}><Phone size={20} /></a>
+            <a href="https://wa.me/254742009497?text=Hello%20AfriVibe%20Safaris%2C%20I'm%20interested%20in%20planning%20a%20safari.%20Could%20you%20please%20provide%20more%20information%3F" target="_blank" rel="noreferrer" aria-label="WhatsApp" style={{ color: "var(--color-primary)" }}><Phone size={20} /></a>
           </div>
         </div>
 
